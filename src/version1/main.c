66529c135f1756fe58de46985046e0381a3d8c94
JoJunHwa <jjj5306@naver.com>
2024-11-18 20:07:34 +0900

    feat : modify Makefile

--- src/version1/main.c
+++ src/version1/main.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include "proxy/proxy.h"
 
 int main() {
     int listen_port = 39071;
     int target_port = 39067;
-    char* target_host = "127.0.0.1";
+    char* target_host = "113.198.138.212";
 
     return run_proxy(listen_port, target_port, target_host);
 }
