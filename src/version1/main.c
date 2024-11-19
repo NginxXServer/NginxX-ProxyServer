b8b18499bfd7debf06511ad7f8a68411bc21a023
JoJunHwa <jjj5306@naver.com>
2024-11-19 13:09:17 +0900

    feat : modify network port

--- src/version1/main.c
+++ src/version1/main.c
@@ -2,7 +2,7 @@
 
 int main() {
     int listen_port = 39071;
-    int target_port = 39067;
+    int target_port = 39076;
     char* target_host = "113.198.138.212";
 
     return run_proxy(listen_port, target_port, target_host);
