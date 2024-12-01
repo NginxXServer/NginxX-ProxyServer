dcba4ca51e0755e520889b6781bba78cf59936ed
JoJunHwa <jjj5306@naver.com>
2024-12-01 23:22:36 +0900

    feat : update v1 target host

--- src/version1/main.c
+++ src/version1/main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-extern int run_proxy(int listen_port, int target_port, char* target_host);
+extern int run_proxy(int listen_port, int target_port, char *target_host);
 
-int main() {
+int main()
+{
     int listen_port = 39071;
     int target_port = 39020;
-    //char* target_host = "10.198.138.212";
-    //char* target_host = "10.198.138.2123";
-    char* target_host = "127.0.0.1";
+    char *target_host = "10.198.138.212";
+    // char* target_host = "10.198.138.2123";
+    // char* target_host = "127.0.0.1";
 
     return run_proxy(listen_port, target_port, target_host);
 }
